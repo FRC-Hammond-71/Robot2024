73fd319567e23aa1428ea2119d68ac07706141d2
your name <kellonisabeast1@gmail.com>
2024-01-10 20:09:14 -0600

    modified:   WPILib/src/main/java/frc/robot/Robot.java 	modified:   WPILib/src/main/java/frc/robot/subsystem/DriveSubsystem.java 	new file:   WPILib/src/main/java/frc/robot/subsystem/LocalizationSubsystem.java

--- WPILib/src/main/java/frc/robot/Robot.java
+++ WPILib/src/main/java/frc/robot/Robot.java
@@ -5,11 +5,13 @@
 package frc.robot;
 
 import frc.robot.LimelightHelpers;
+import frc.robot.subsystem.DriveSubsystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.cameraserver.CameraServer;
 //import edu.wpi.first.wpilibj2.command.Command;
@@ -40,25 +42,11 @@ import edu.wpi.first.wpilibj.SPI;
 
 
 public class Robot extends TimedRobot {
-  private CANSparkMax m_leftLeadMotor;
-  private CANSparkMax m_rightLeadMotor;
-  private CANSparkMax m_leftFollowMotor;
-  private CANSparkMax m_rightFollowMotor;
 
-  private CANSparkMax m_armMotor;
-  private CANSparkMax m_intakeMotor;
+  private XboxController m_driverController = new XboxController(0);
+  private XboxController m_operatorController = new XboxController(1);
 
-  private DifferentialDrive m_robotDrive;
-
-  private XboxController m_driverController;
-  private XboxController m_operatorController;
-
-  private RelativeEncoder m_leftLeadEncoder;
-  private RelativeEncoder m_rightLeadEncoder;
-  private RelativeEncoder m_leftFollowEncoder;
-  private RelativeEncoder m_rightFollowEncoder;
-  private RelativeEncoder m_armEncoder;
-  private RelativeEncoder m_intakeEncoder;
+  private DriveSubsystem m_drive;
 
   /*private AbsoluteEncoder m_armAbsoluteEncoder;*/
 
@@ -77,48 +65,16 @@ public class Robot extends TimedRobot {
   private double startTime;
 
 
+  public Robot()
+  {
+    super();
+    this.m_drive = new DriveSubsystem();
+  }
+
   @Override
   public void robotInit() {
-   
-
-    
-    // motors and encoders
-    m_leftLeadMotor = new CANSparkMax(5,MotorType.kBrushless);
-    m_rightLeadMotor = new CANSparkMax(4,MotorType.kBrushless);
-    m_leftFollowMotor = new CANSparkMax(3,MotorType.kBrushless);
-    m_rightFollowMotor = new CANSparkMax(2,MotorType.kBrushless);
-
-    m_armMotor = new CANSparkMax(6,MotorType.kBrushless);
-    m_intakeMotor = new CANSparkMax(7, MotorType.kBrushless);
-
-    m_leftLeadMotor.restoreFactoryDefaults();
-    m_rightLeadMotor.restoreFactoryDefaults();
-    m_leftFollowMotor.restoreFactoryDefaults();
-    m_rightFollowMotor.restoreFactoryDefaults();
-    m_armMotor.restoreFactoryDefaults();
-    m_intakeMotor.restoreFactoryDefaults();
-
-    m_rightLeadMotor.setInverted(true);
-
-    m_leftFollowMotor.follow(m_leftLeadMotor);
-    m_rightFollowMotor.follow(m_rightLeadMotor);
-
-    m_leftLeadEncoder = m_leftLeadMotor.getEncoder();
-    m_rightLeadEncoder = m_rightLeadMotor.getEncoder();
-    m_leftFollowEncoder = m_leftFollowMotor.getEncoder();
-    m_rightFollowEncoder = m_rightFollowMotor.getEncoder();
-
-    m_armEncoder = m_armMotor.getEncoder();
-    m_intakeEncoder = m_intakeMotor.getEncoder();
-    /*m_armAbsoluteEncoder = m_armMotor.getAbsoluteEncoder(Type.kDutyCycle);*/
-    
-
-    m_robotDrive = new DifferentialDrive(m_leftLeadMotor, m_rightLeadMotor);
-
-    m_driverController = new XboxController(0);
-    m_operatorController = new XboxController(1);
-
-    m_robotDrive.setDeadband(0.05);
+        
+    // m_robotDrive.setDeadband(0.05);
 
     m_chooser.setDefaultOption("Cone DE Auto", kDefaultAuto);
     m_chooser.addOption("Cube DE Auto", kCustomAuto);
@@ -136,32 +92,23 @@ public class Robot extends TimedRobot {
       /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
 
       ahrs = new AHRS(SPI.Port.kMXP); 
-
-      CameraServer.startAutomaticCapture();   
-
-      
-      
-      
+      CameraServer.startAutomaticCapture();
 
       //Limelight initiation Code
       LLTable.getEntry("camMode").setNumber(0);
-  
- 
 
-
-}
-
+      CommandScheduler.getInstance().registerSubsystem(m_drive);
+  }
 
   @Override
   public void robotPeriodic() {
     /*SmartDashboard.putNumber("Arm Encoder Position", m_armAbsoluteEncoder.getPosition());*/
-    SmartDashboard.putNumber("Left Lead Motor Encoder", m_leftLeadEncoder.getPosition()*kDriveTick2Feet);
-    SmartDashboard.putNumber("Right Lead Motor Encoder", m_rightLeadEncoder.getPosition()*kDriveTick2Feet);
-    SmartDashboard.putNumber("Left Follow Motor Encoder", m_leftFollowEncoder.getPosition()*kDriveTick2Feet);
-    SmartDashboard.putNumber("Right Follow Motor Encoder", m_rightFollowEncoder.getPosition()*kDriveTick2Feet);
-    SmartDashboard.putNumber("Right Follow Motor Encoder", m_rightFollowEncoder.getPosition()*kDriveTick2Feet);
-   
-
+    // SmartDashboard.putNumber("Left Lead Motor Encoder", m_leftLeadEncoder.getPosition()*kDriveTick2Feet);
+    // SmartDashboard.putNumber("Right Lead Motor Encoder", m_rightLeadEncoder.getPosition()*kDriveTick2Feet);
+    // SmartDashboard.putNumber("Left Follow Motor Encoder", m_leftFollowEncoder.getPosition()*kDriveTick2Feet);
+    // SmartDashboard.putNumber("Right Follow Motor Encoder", m_rightFollowEncoder.getPosition()*kDriveTick2Feet);
+    // SmartDashboard.putNumber("Right Follow Motor Encoder", m_rightFollowEncoder.getPosition()*kDriveTick2Feet);
+    
     /* Display 6-axis Processed Angle Data                                      */
     SmartDashboard.putBoolean(  "IMU_Connected",        ahrs.isConnected());
     SmartDashboard.putBoolean(  "IMU_IsCalibrating",    ahrs.isCalibrating());
@@ -169,16 +116,13 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber(   "IMU_Pitch",            ahrs.getPitch());
     SmartDashboard.putNumber(   "IMU_Roll",             ahrs.getRoll());
 
+    SmartDashboard.putString("GetChassisSpeeds()", this.m_drive.GetChassisSpeeds().toString());
    
-  
     //read values periodically
     NetworkTableEntry Tx = LLTable.getEntry("tx");
     NetworkTableEntry Ty = LLTable.getEntry("ty");
     NetworkTableEntry Ta = LLTable.getEntry("ta");
     NetworkTableEntry Tv = LLTable.getEntry("tv");
-    
-
-    
 
     //post to smart dashboard periodically
     SmartDashboard.putBoolean("Targets", LimelightHelpers.getTV("limelight"));
@@ -189,297 +133,20 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousInit() {
-    startTime = Timer.getFPGATimestamp();
-    m_leftLeadEncoder.setPosition(0);
-    m_rightLeadEncoder.setPosition(0);
-    m_rightFollowEncoder.setPosition(2000);
-    m_autoSelected = m_chooser.getSelected();
-    System.out.println("Auto selected: " + m_autoSelected);
-    
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-   double time = Timer.getFPGATimestamp();
-   double leftPosition = m_leftLeadEncoder.getPosition() * kDriveTick2Feet;
-   double rightPosition = m_rightLeadEncoder.getPosition() * kDriveTick2Feet;
-   double thirdDistance = m_rightFollowEncoder.getPosition();
-   double distance = (leftPosition + rightPosition) / 2;
-   double imuRoll = ahrs.getRoll();
-   int eventID = 0;
-   SmartDashboard.putNumber("EventID",     eventID);
-  //AUTO: score cone, grab block, balance
-   switch (m_autoSelected) {
-    case kDefaultAuto:
-     if (time - startTime < 1) {
-      m_armMotor.set(-0.5);
-  
-     } else if (time - startTime < 1.5) {
-      m_armMotor.set(0);
-      m_intakeMotor.set(-0.2);
-  //score cone
-     } else if (time - startTime < 3) {
-      m_intakeMotor.set(0);
-     // eventID += 1;
-      m_armMotor.set(0.5);
-      m_robotDrive.tankDrive(0.5, 0.5);
-  
-     } else if (time - startTime < 4) {
-      m_armMotor.set(0);
-      m_robotDrive.tankDrive(0.5, 0.5);
-  //grab blocc
-     } else if (time - startTime < 5.6) {
-      m_armMotor.set(0.5);
-      m_robotDrive.tankDrive(0.5, 0.5);
-
-     } else if (time - startTime < 6.5) {
-        m_robotDrive.tankDrive(0.5, 0.5);
-        m_armMotor.set(0);
-        m_intakeMotor.set(-1);
-  
-     } else if (time - startTime < 9) {
-        m_robotDrive.tankDrive(-0.5, -0.5);
-       // eventID += 1;
-        m_armMotor.set(-0.4);
-        m_intakeMotor.set(0);
-
-      } else if (time - startTime < 9.5) {
-        m_robotDrive.tankDrive(-0.5, -0.5);
-       // eventID += 1;
-        m_armMotor.set(0);
-        //m_intakeMotor.set(0);
-
-
-  //BALANCE ==================================================================
-     } else if (imuRoll < -11) {
-      m_robotDrive.tankDrive(-0.3, -0.3);
-     // m_armMotor.set(0);
-     } else if (imuRoll < 11 && imuRoll > 0) {
-      m_robotDrive.tankDrive(0, 0);
-  
-     } else if (imuRoll < -11) {
-      m_robotDrive.tankDrive(-0.3, -0.3);
-  
-     } else if (imuRoll > 0 && imuRoll < 11) {
-      m_robotDrive.tankDrive(0, 0);
-  
-     } else if (imuRoll > 11) {
-      m_robotDrive.tankDrive(0.3, 0.3);
-   //FAILSAFE FOR ARM ========================================================
-     } else if (time - startTime < 12)  {
-         m_armMotor.set(0);
-     }
-      break;
-    case kCustomAuto:
-    default:
-    if (time - startTime < 1) {
-      m_armMotor.set(-0.7);
-  
-     } else if (time - startTime < 1.5) {
-      m_armMotor.set(0);
-      m_intakeMotor.set(1);
-  
-     } else if (time - startTime < 3) {
-      m_intakeMotor.set(0);
-      m_armMotor.set(0.7);
-  
-     } else if (time - startTime < 4) {
-      m_armMotor.set(0);
-  
-     } else if (time - startTime < 8.5) {
-      m_robotDrive.tankDrive(0.5, 0.5);
-  
-     } else if (time - startTime < 11) {
-      m_robotDrive.tankDrive(-0.5, -0.5);
-  
-     } else if (imuRoll < -11) {
-      m_robotDrive.tankDrive(-0.3, -0.3);
-  
-     } else if (imuRoll < 11 && imuRoll > 0) {
-      m_robotDrive.tankDrive (0, 0);
-  
-     } else if (imuRoll < -11) {
-      m_robotDrive.tankDrive (-0.3, -.3);
-  
-     } else if (imuRoll > 0 && imuRoll < 11) {
-      m_robotDrive.tankDrive (0, 0);
-  
-     } else if (imuRoll > 11) {
-      m_robotDrive.tankDrive (0.3, 0.3);
-     }
-      break;
-  // FIRST AUTO CODE ====================================================
-  /*  if (distance < 37) {
-    m_robotDrive.tankDrive(0.4, 0.4);
-   } else if (imuRoll > 11) {
-    m_robotDrive.tankDrive(0.3, 0.3);
-   } else if (imuRoll < 11 && imuRoll > 0) {
-    m_robotDrive.tankDrive (0, 0);
-   } else if (imuRoll < 0) {
-    m_robotDrive.tankDrive (-0.25, -0.25);
-   } else if (imuRoll > 0) {
-    m_robotDrive.tankDrive (0, 0);
-   }
-   +/
-  
-   // SECOND AUTO CODE ==================================================
-   /* if (time - startTime < 1) {
-    m_armMotor.set(-0.5);
-
-   } else if (time - startTime < 1.5) {
-    m_armMotor.set(0);
-    m_intakeMotor.set(1);
-
-   } else if (time - startTime < 3) {
-    m_intakeMotor.set(0);
-    m_armMotor.set(0.5);
-
-   } else if (time - startTime < 4) {
-    m_armMotor.set(0);
-
-   } else if (time - startTime < 6.1) {
-    m_leftLeadMotor.set(0.3);
-    m_rightLeadMotor.set(0.3);
-  
-   } else if (time - startTime < 6.2) {
-    m_leftLeadMotor.set(-0.5);
-    m_rightLeadMotor.set(-0.5);
-    /*m_leftFollowMotor.set(0.5);
-    m_rightFollowMotor.set(0.5);*/
-
-    // THIRD AUTO CODE =======================================================
-   /*if (time - startTime < 1) {
-    m_armMotor.set(-0.5);
-
-   } else if (time - startTime < 1.5) {
-    m_armMotor.set(0);
-    m_intakeMotor.set(1);
-
-   } else if (time - startTime < 3) {
-    m_intakeMotor.set(0);
-    m_armMotor.set(0.5);
-
-   } else if (time - startTime < 4) {
-    m_armMotor.set(0);
-
-/*time - startTime < 6.2
-
-   } else if (IMU_Roll > -12.5) {
-    m_leftLeadMotor.set(0.3);
-    m_rightLeadMotor.set(0.3);
-  
- /*time - startTime < 6.4
-   } else if () {
-    m_leftLeadMotor.set(-0.15);
-    m_rightLeadMotor.set(0.15); 
-    
-   } else if (time - startTime < 6.6) {
-    m_armMotor.set(0.15);
-
-   } else if (time - startTime < 6.7) {
-    m_leftLeadMotor.set(0.2);
-    m_rightLeadMotor.set(0.2);
-
-   } else if (time - startTime < 7){
-    m_intakeMotor.set(-0.5);
-
-   } else if (time - startTime < 7.1) {
-    m_intakeMotor.set(0);
-
-   }*/ 
-  }
-}   
-
-
-  @Override
   public void teleopInit() {
-    m_leftLeadMotor.setIdleMode(IdleMode.kCoast);
-    m_rightLeadMotor.setIdleMode(IdleMode.kCoast);
-    m_leftFollowMotor.setIdleMode(IdleMode.kCoast);
-    m_rightFollowMotor.setIdleMode(IdleMode.kCoast);
+
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX()*0.7);
 
-    double armPower = m_operatorController.getLeftY();
-    
-    armPower *= 0.8;
-
-    m_armMotor.set(armPower);
-
-    double tx = LimelightHelpers.getTX("limelight");
-    double ty = LimelightHelpers.getTY("limelight");
-
-
-    if(m_operatorController.getAButtonPressed() == true)
-    {
-      m_intakeMotor.set(-0.85);
-    }
-    else if(m_operatorController.getAButtonReleased() == true)
-    {
-      m_intakeMotor.stopMotor();
-    }
-    if(m_operatorController.getBButtonPressed() == true)
-    {
-      m_intakeMotor.set(1);
-    }
-    else if(m_operatorController.getBButtonReleased() == true)
-    {
-      m_intakeMotor.stopMotor();
-    }
-    if(m_operatorController.getYButtonPressed() == true)
-    {
-      m_intakeMotor.set(-0.35);
-    }
-    else if(m_operatorController.getYButtonReleased() == true)
-    {
-      m_intakeMotor.stopMotor();
-    }
-    if(m_operatorController.getXButtonPressed() == true)
-    {
-      m_intakeMotor.set(0.2);
-    }
-    else if(m_operatorController.getXButtonReleased() == true)
-    {
-      m_intakeMotor.stopMotor();
-    }
-    if(m_operatorController.getRightBumperPressed() == true)
-    {
-      m_intakeMotor.set(-0.2);
-    }
-    else if(m_operatorController.getRightBumperReleased() == true)
-    {
-      m_intakeMotor.stopMotor();
-    }
-
-    if(m_driverController.getLeftBumperPressed() == true)
-    {
-      if(LimelightHelpers.getFiducialID("limelight") == 8)
-      {
-        if(tx < 0.20)
-        {
-          m_robotDrive.tankDrive(0.5, -0.5);
-        }
-        else if(tx > 0.20)
-        {
-          m_robotDrive.tankDrive(0.5, -0.5);
-        }
-      }
-    }
-
-
+    m_drive.Drive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX()*0.7);
 
     }
 
   @Override
   public void disabledInit() {
-    m_leftLeadMotor.setIdleMode(IdleMode.kBrake);
-    m_rightLeadMotor.setIdleMode(IdleMode.kBrake);
-    m_leftFollowMotor.setIdleMode(IdleMode.kBrake);
-    m_rightFollowMotor.setIdleMode(IdleMode.kBrake);
+
   }
 
   @Override
@@ -490,16 +157,16 @@ public class Robot extends TimedRobot {
     double startTime = Timer.getFPGATimestamp();
   }
 
-  @Override
-  public void testPeriodic() {
-    double time = Timer.getFPGATimestamp(); 
+  // @Override
+  // public void testPeriodic() {
+  //   double time = Timer.getFPGATimestamp(); 
     
-    if(time - startTime < 1) {
-      m_intakeMotor.set(1);
-    } else {
-      m_armMotor.set(0);
-    }
-  }
+  //   if(time - startTime < 1) {
+  //     m_intakeMotor.set(1);
+  //   } else {
+  //     m_armMotor.set(0);
+  //   }
+  // }
 
   @Override
   public void simulationInit() {}
--- WPILib/src/main/java/frc/robot/subsystem/DriveSubsystem.java
+++ WPILib/src/main/java/frc/robot/subsystem/DriveSubsystem.java
@@ -1,31 +1,79 @@
 package frc.robot.subsystem;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.ReplanningConfig;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.*;
+import java.util.Date;
 
 public class DriveSubsystem extends SubsystemBase {
-    public DriveSubsystem() {
-        // All other subsystem initialization
-        // ...
-        // Configure AutoBuilder last
-        // AutoBuilder.configureRamsete(
-        //         this::getPose, // Robot pose supplier
-        //         this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
-        //         this::getCurrentSpeeds, // Current ChassisSpeeds supplier
-        //         this::drive, // Method that will drive the robot given ChassisSpeeds
-        //         new ReplanningConfig(), // Default path replanning config. See the API for the options here
-        //         () -> {
-        //             // Boolean supplier that controls when the path will be mirrored for the red alliance
-        //             // This will flip the path being followed to the red side of the field.
-        //             // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-        //             var alliance = DriverStation.getAlliance();
-        //             if (alliance.isPresent()) {
-        //                 return alliance.get() == DriverStation.Alliance.Red;
-        //             }
-        //             return false;
-        //         },
-        //         this // Reference to this subsystem to set requirements
-        // );
+    // ------
+    // Motors
+    // ------ 
+    private CANSparkMax LeftLeadMotor = new CANSparkMax(5,MotorType.kBrushless);
+    private CANSparkMax RightLeadMotor = new CANSparkMax(4,MotorType.kBrushless);
+    private CANSparkMax LeftFollowMotor = new CANSparkMax(3,MotorType.kBrushless);
+    private CANSparkMax RightFollowMotor =  new CANSparkMax(2,MotorType.kBrushless);
+    private CANSparkMax ArmMotor = new CANSparkMax(6,MotorType.kBrushless);
+    /**
+     * Intake & Launcher Motor
+     */
+    public CANSparkMax IntakeMotor = new CANSparkMax(7, MotorType.kBrushless);
+
+    public DifferentialDrive Drive = new DifferentialDrive(LeftLeadMotor, RightLeadMotor);
+
+    public DriveSubsystem() { }
+
+    // ------------------
+    // Movement Reporting
+    // ------------------
+    /**
+     * Left Motor Velocity in Seconds.
+     */
+    public double GetLeftMotorVelocity()
+    {
+        return this.LeftLeadMotor.getEncoder().getVelocity();
+    }
+    /**
+     * Right Motor Velocity in Seconds.
+     */
+    public double GetRightMotorVelocity()
+    {
+        return this.LeftLeadMotor.getEncoder().getVelocity();
+    }
+
+    public ChassisSpeeds GetChassisSpeeds()
+    {
+        var differential_drive_kinematics = new DifferentialDriveKinematics(20);
+
+        // Wheel Specs:
+        // Radius = 3 in
+        // Diameter = Radius * 2 = 6 in
+        // Circumference = Diameter * PI = 18.85 inches = 0.48 meters
+
+        // Requires meters per second.
+        var differential_drive_wheel_speeds = new DifferentialDriveWheelSpeeds(
+            GetLeftMotorVelocity() / 60 * 0.48,
+            GetRightMotorVelocity() / 60 * 0.48
+        );
+
+        return differential_drive_kinematics.toChassisSpeeds(differential_drive_wheel_speeds);
+    }
+
+    @Override
+    public void periodic() 
+    {
+        SmartDashboard.putString("GetChassisSpeeds()", GetChassisSpeeds().toString());
+        
+        // SmartDashboard.putString("Left Velocity", String.format("%n RPM", GetLeftMotorVelocity()));
+        // SmartDashboard.putString("Right Velocity", String.format("%n RPM", GetRightMotorVelocity()));
     }
 }
